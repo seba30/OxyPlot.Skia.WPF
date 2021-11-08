e3981de9adedc479ecf4d76ecc9a1571e51bcce4
Sebastian Dreyßig <s.dreyssig@kathrein-sachsen.de>
2021-11-08 07:08:45 +0100

    Add some components to chart

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using OxyPlot;
+using OxyPlot.Axes;
+
+namespace SkiaSharpTest
+{
+	public class MainWindowViewModel
+	{
+		public PlotModel PlotViewModel { get; }
+
+		public MainWindowViewModel()
+		{
+			PlotViewModel = new PlotModel
+							{
+								Title = "Sample chart"
+							};
+
+			PlotViewModel.Axes.Add(new LinearAxis
+								   {
+									   Position = AxisPosition.Left,
+									   Title = "Left axis"
+								   });
+
+			PlotViewModel.Axes.Add(new LinearAxis
+								   {
+									   Position = AxisPosition.Bottom,
+									   Title = "Bottom axis"
+								   });
+		}
+	}
+}
